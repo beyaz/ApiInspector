00ea0e14d51494db8ef4fc982308e95396281c9a
Abdullah Beyaztaş <beyaz1404@gmail.com>
2020-07-14 15:32:02 +0300

    .

--- ApiInspector.Test/UnitTest1.cs
+++ ApiInspector.Test/UnitTest1.cs
@@ -1,14 +1,46 @@
 ﻿using System;
+using ApiInspector.Components;
+using ApiInspector.DataAccess;
+using ApiInspector.Models;
+using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace ApiInspector.Test
 {
+
+    class AnyClass
+    {
+        public string AnyMethod_0()
+        {
+            return "0";
+        }
+    }
+
+
     [TestClass]
-    public class UnitTest1
+    public class InvocationTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Should_invoke_non_static_parameterless_method()
         {
+            var builder = new InvocationInfoEditorContextBuilder();
+
+            var context = builder.Build();
+
+            var invocationInfo = new InvocationInfo()
+            {
+                AssemblyName = "ApiInspector.Test.dll",
+                ClassName    = "ApiInspector.Test.AnyClass",
+                MethodName   = "AnyMethod_0"
+            };
+
+            context.Update(DataKeys.InvocationInfo,invocationInfo);
+
+            Logic.Execute(context);
+
+            var response = context.Get(DataKeys.ExecutionResponse);
+
+            response.Should().Be("0");
         }
     }
 }
--- ApiInspector/DataAccess/DataKeys.cs
+++ ApiInspector/DataAccess/DataKeys.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows.Controls;
+using ApiInspector.Models;
 using BOA.DataFlow;
 using Mono.Cecil;
 
@@ -7,23 +8,21 @@ namespace ApiInspector.DataAccess
     static class DataKeys
     {
         #region Static Fields
-
-        public static readonly DataKey<string> TargetEnvironment = new DataKey<string>(nameof(TargetEnvironment));
-
-        public static readonly DataKey<object> ExecutionResponse = new DataKey<object>(nameof(ExecutionResponse));
-
         public static readonly DataKey<string> AssemblyFilePath = new DataKey<string>(nameof(AssemblyFilePath));
-        public static readonly DataKey<string> AssemblyName = new DataKey<string>(nameof(AssemblyName));
-        public static readonly DataKey<string> ClassName    = new DataKey<string>(nameof(ClassName));
-        public static readonly DataKey<string> MethodName   = new DataKey<string>(nameof(MethodName));
-
-        public static  readonly DataKey<MethodDefinition> MethodDefinition = new DataKey<MethodDefinition>(nameof(MethodDefinition));
+        public static readonly DataKey<string> AssemblyName     = new DataKey<string>(nameof(AssemblyName));
 
         public static readonly DataKey<string> AssemblySearchDirectory = new DataKey<string>(nameof(AssemblySearchDirectory));
+        public static readonly DataKey<string> ClassName               = new DataKey<string>(nameof(ClassName));
+
+        public static readonly DataKey<object>         ExecutionResponse = new DataKey<object>(nameof(ExecutionResponse));
+        public static readonly DataKey<InvocationInfo> InvocationInfo    = new DataKey<InvocationInfo>(nameof(InvocationInfo));
+
+        public static readonly DataKey<MethodDefinition> MethodDefinition = new DataKey<MethodDefinition>(nameof(MethodDefinition));
+        public static readonly DataKey<string>           MethodName       = new DataKey<string>(nameof(MethodName));
+
+        public static readonly DataKey<StackPanel> ParametersPanel = new DataKey<StackPanel>(nameof(ParametersPanel));
+
+        public static readonly DataKey<string> TargetEnvironment = new DataKey<string>(nameof(TargetEnvironment));
         #endregion
-
-        public static DataKey<StackPanel> ParametersPanel = new DataKey<StackPanel>(nameof(ParametersPanel));
     }
-
-
 }
