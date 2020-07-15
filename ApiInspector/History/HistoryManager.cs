ac038f95c74241c05fbcd6e90496e123c195985d
Abdullah Beyaztaş <beyaz1404@gmail.com>
2020-07-15 23:17:53 +0300

    .

--- ApiInspector/History/HistoryManager.cs
+++ ApiInspector/History/HistoryManager.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using System.IO;
-using ApiInspector.MainWindow;
 using ApiInspector.Models;
 using BOA.DataFlow;
 using Newtonsoft.Json;
@@ -13,37 +12,9 @@ namespace ApiInspector.History
         public static DataKey<IReadOnlyList<InvocationInfo>> HistoryDataKey = new DataKey<IReadOnlyList<InvocationInfo>>(nameof(HistoryDataKey));
         #endregion
 
-        static string DirectoryPath => Path.GetDirectoryName(typeof(HistoryManager).Assembly.Location) + Path.DirectorySeparatorChar + nameof(ApiInspector)+"History"+Path.DirectorySeparatorChar;
-
-        public static void SaveToHistory(InvocationInfo info)
-        {
-            var filePath = Path.Combine(DirectoryPath, info.ToString().Replace(":", "____") + ".json");
-
-            if (!Directory.Exists(DirectoryPath))
-            {
-                Directory.CreateDirectory(DirectoryPath);
-            }
-
-            File.WriteAllText(filePath,SerializeToJson(info));
-        }
-
-         static string SerializeToJson(object value)
-        {
-            if (value == null)
-            {
-                return null;
-            }
-
-            var settings = new JsonSerializerSettings
-            {
-                DefaultValueHandling = DefaultValueHandling.Ignore ,
-                Formatting           = Formatting.Indented,
-                DateFormatString     = "yyyy.MM.dd hh:mm:ss",
-               TypeNameHandling = TypeNameHandling.Objects
-            };
-
-            return JsonConvert.SerializeObject(value, settings);
-        }
+        #region Properties
+        static string DirectoryPath => Path.GetDirectoryName(typeof(HistoryManager).Assembly.Location) + Path.DirectorySeparatorChar + nameof(ApiInspector) + "History" + Path.DirectorySeparatorChar;
+        #endregion
 
         #region Public Methods
         public static IReadOnlyList<InvocationInfo> GetHistory(DataContext context)
@@ -103,6 +74,38 @@ namespace ApiInspector.History
                     }
                 }
             };
+        }
+
+        public static void SaveToHistory(InvocationInfo info)
+        {
+            var filePath = Path.Combine(DirectoryPath, info.ToString().Replace(":", "____") + ".json");
+
+            if (!Directory.Exists(DirectoryPath))
+            {
+                Directory.CreateDirectory(DirectoryPath);
+            }
+
+            File.WriteAllText(filePath, SerializeToJson(info));
+        }
+        #endregion
+
+        #region Methods
+        static string SerializeToJson(object value)
+        {
+            if (value == null)
+            {
+                return null;
+            }
+
+            var settings = new JsonSerializerSettings
+            {
+                DefaultValueHandling = DefaultValueHandling.Ignore,
+                Formatting           = Formatting.Indented,
+                DateFormatString     = "yyyy.MM.dd hh:mm:ss",
+                TypeNameHandling     = TypeNameHandling.Objects
+            };
+
+            return JsonConvert.SerializeObject(value, settings);
         }
         #endregion
     }
