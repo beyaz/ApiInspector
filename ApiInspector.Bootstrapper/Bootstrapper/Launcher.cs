85663f7546bba18ce066e5f01391bb733b094480
Abdullah Beyaztaş <beyaz1404@gmail.com>
2020-07-22 19:42:55 +0300

    .

--- ApiInspector.Bootstrapper/Bootstrapper/Launcher.cs
+++ ApiInspector.Bootstrapper/Bootstrapper/Launcher.cs
@@ -1,74 +1,123 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Diagnostics;
 using System.IO;
-using System.IO.Compression;
+using System.Linq;
+using BOA.DataFlow;
+using Dapper;
+using Dapper.Contrib.Extensions;
 
 namespace ApiInspector.Bootstrapper
 {
+    /// <summary>
+    ///     The file model
+    /// </summary>
+    [Table("[WHT].[File]")]
+    class FileModel
+    {
+        #region Public Properties
+        /// <summary>
+        ///     Gets or sets the name of the application.
+        /// </summary>
+        [ExplicitKey]
+        public string ApplicationName { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the content.
+        /// </summary>
+        public byte[] Content { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the key.
+        /// </summary>
+        [ExplicitKey]
+        public string FullName { get; set; }
+        #endregion
+    }
+
+    /// <summary>
+    ///     The key
+    /// </summary>
+    class Key
+    {
+        #region Static Fields
+        /// <summary>
+        ///     The files
+        /// </summary>
+        public static DataKey<IReadOnlyList<FileModel>> Files = new DataKey<IReadOnlyList<FileModel>>(nameof(Files));
+
+        /// <summary>
+        ///     The target directory path
+        /// </summary>
+        public static DataKey<string> TargetDirectoryPath = new DataKey<string>(nameof(TargetDirectoryPath));
+        #endregion
+    }
+
+    /// <summary>
+    ///     The launcher
+    /// </summary>
     class Launcher
     {
-        #region Fields
-        readonly Config config;
-        readonly string ZipFileTempPath;
-        #endregion
-
-        #region Constructors
-        public Launcher()
-        {
-            config          = Config.CreateFromFile();
-            ZipFileTempPath = config.ZipFilePath + ".tmp";
-        }
-        #endregion
-
         #region Public Methods
+        /// <summary>
+        ///     Starts this instance.
+        /// </summary>
         public void Start()
         {
             Console.WriteLine("Yükleniyor...");
 
-            DownloadFile();
+            var context = new DataContext
+            {
+                {Key.TargetDirectoryPath, @"d:\boa\server\bin\"}
+            };
 
-            Console.WriteLine("Yüklendi.");
+            FetchFiles(context);
 
-            File.Delete(config.ZipFilePath);
+            ExportFiles(context);
 
-            File.Move(ZipFileTempPath, config.ZipFilePath);
-
-            TryToExtractZipFile();
-
-            Console.WriteLine("Starting process...");
-
-            File.WriteAllText(config.ExportDir + "Run.bat", config.BatFileContent);
-
-            Process.Start(config.ProcessPath);
+            StartProcess(context);
 
             Console.WriteLine("Process is started.");
         }
         #endregion
 
         #region Methods
-        void DownloadFile()
+        /// <summary>
+        ///     Downloads the files.
+        /// </summary>
+        static void ExportFiles(DataContext context)
         {
-            //TFSAccessForBOA.DownloadFile(config.ZipFileUrl, ZipFileTempPath);
+            var targetDir = context.Get(Key.TargetDirectoryPath);
+
+            foreach (var file in context.Get(Key.Files))
+            {
+                var path = Path.Combine(targetDir, file.FullName);
+
+                File.WriteAllBytes(path, file.Content);
+            }
         }
 
-        void TryToExtractZipFile()
+        /// <summary>
+        ///     Fetches the files.
+        /// </summary>
+        static void FetchFiles(DataContext context)
         {
-            if (File.Exists(config.ZipFilePath))
-            {
-                if (Directory.Exists(config.ExportDir))
-                {
-                    Console.WriteLine("Deleting folder: " + config.ExportDir);
-                    Directory.Delete(config.ExportDir, true);
-                    Console.WriteLine("Deleted folder: " + config.ExportDir);
-                }
+            const string ConnectionString = "server=srvdev\\ATLAS;database =BOA;integrated security=true";
 
-                Console.WriteLine("Exporting zip file: " + config.ZipFilePath);
-                ZipFile.ExtractToDirectory(config.ZipFilePath, config.ExportDir);
+            var connection = new SqlConnection(ConnectionString);
 
-                Console.WriteLine("Deleting zip file: " + config.ZipFilePath);
-                File.Delete(config.ZipFilePath);
-                Console.WriteLine("Deleted zip file: " + config.ZipFilePath);
-            }
+            context.Add(Key.Files, connection.Query<FileModel>("SELECT * FROM  [WHT].[File] WITH(NOLOCK) WHERE ApplicationName = 'ApiInspector'").ToList().AsReadOnly());
+        }
+
+        /// <summary>
+        ///     Starts the process.
+        /// </summary>
+        static void StartProcess(DataContext context)
+        {
+            var targetDir = context.Get(Key.TargetDirectoryPath);
+
+            Process.Start(Path.Combine(targetDir, "Run.bat"));
         }
         #endregion
     }
