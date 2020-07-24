f26e67368b31adcfe4541fca2520c994f8a3439b
Abdullah Beyaztaş <beyaz1404@gmail.com>
2020-07-25 01:36:15 +0300

    .

---
+++ ApiInspector/MainWindow/HistoryPanel.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ApiInspector.MainWindow
+{
+    /// <summary>
+    /// Interaction logic for HistoryPanel.xaml
+    /// </summary>
+    public partial class HistoryPanel : UserControl
+    {
+        public HistoryPanel()
+        {
+            InitializeComponent();
+        }
+
+        void HistoryFilterTextBox_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        void HistoryListBox_OnSelected(object sender, SelectionChangedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        void HistoryListBox_OnKeyDown(object sender, KeyEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
