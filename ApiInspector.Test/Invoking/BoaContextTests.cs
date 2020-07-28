0fb3a41732f6a6f6f13c439301bb1aeeb1378f0c
Abdullah Beyaztaş <beyaz1404@gmail.com>
2020-07-28 17:22:47 +0300

    .

---
+++ ApiInspector.Test/Invoking/BoaContextTests.cs
@@ -0,0 +1,22 @@
+﻿using ApiInspector.MainWindow;
+using ApiInspector.Tracing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ninject;
+
+namespace ApiInspector.Invoking
+{
+    [TestClass]
+    public class BoaContextTests
+    {
+        [TestMethod]
+        public void Authenticate()
+        {
+            using (var injector = new Injector(new TraceQueue(),"Dev"))
+            {
+                var boaContext = injector.Get<BOAContext>();
+                boaContext.Context.ToString();
+
+            }
+        }
+    }
+}
