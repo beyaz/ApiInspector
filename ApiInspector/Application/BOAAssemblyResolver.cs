afc16d455735eccde4681c3926b2e87000044b28
Abdullah Beyaztaş <beyaz1404@gmail.com>
2021-01-26 17:56:49 +0300

    .

--- ApiInspector/Application/BOAAssemblyResolver.cs
+++ ApiInspector/Application/BOAAssemblyResolver.cs
@@ -22,34 +22,7 @@ namespace ApiInspector.Application
         #endregion
 
         #region Methods
-        /// <summary>
-        ///     Finds the assembly.
-        /// </summary>
-        internal static Assembly FindAssembly(string assemblyFileNameWithoutExtension)
-        {
-            var trace = fun((string message) => Console.WriteLine(message));
-
-            trace($"Trying to find assembly: {assemblyFileNameWithoutExtension}");
-
-            if (assemblyFileNameWithoutExtension == "BOA.Integration.Connector")
-            {
-                assemblyFileNameWithoutExtension += ".ModifiedVersionForApiInspector";
-            }
-
-            foreach (var searchDirectory in GetDirectories())
-            {
-                var filePath = $@"{searchDirectory}\{assemblyFileNameWithoutExtension}.dll";
-                if (File.Exists(filePath))
-                {
-                    trace($"Loading assembly: {filePath}");
-                    return Assembly.Load(File.ReadAllBytes(filePath));
-                }
-            }
-
-            trace($"Assembly Not Found: {assemblyFileNameWithoutExtension}");
-
-            return null;
-        }
+        
 
         /// <summary>
         ///     Domains the assembly resolve.
@@ -63,7 +36,7 @@ namespace ApiInspector.Application
                 name = name.Substring(0, index);
             }
 
-            var assembly = FindAssembly(name);
+            var assembly = _.FindAssembly(name);
             if (assembly != null)
             {
                 return assembly;
@@ -72,20 +45,7 @@ namespace ApiInspector.Application
             throw new ArgumentException("AssemblyNotFound:" + args.Name);
         }
 
-        /// <summary>
-        ///     Gets the directories.
-        /// </summary>
-        static IReadOnlyList<string> GetDirectories()
-        {
-            return new List<string>
-            {
-                @"D:\BOA\server\bin",
-                @"D:\BOA\client\bin",
-                @"D:\BOA\client\bin\en",
-                @"D:\BOA\HSM\server\bin",
-                @"D:\BOA\BOA.Integration\server\bin"
-            };
-        }
+        
         #endregion
     }
 }
--- ApiInspector/_TypeFinding.cs
+++ ApiInspector/_TypeFinding.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using ApiInspector.Application;
+using static FunctionalPrograming.FPExtensions;
 
 namespace ApiInspector
 {
@@ -12,6 +15,42 @@ namespace ApiInspector
     {
 
         #region Public Methods
+        
+        
+        public static Assembly FindAssembly(string assemblyFileNameWithoutExtension)
+        {
+            var trace = fun((string message) => Console.WriteLine(message));
+
+            trace($"Trying to find assembly: {assemblyFileNameWithoutExtension}");
+
+            if (assemblyFileNameWithoutExtension == "BOA.Integration.Connector")
+            {
+                assemblyFileNameWithoutExtension += ".ModifiedVersionForApiInspector";
+            }
+
+            var searchDirectories = new List<string>
+            {
+                @"D:\BOA\server\bin",
+                @"D:\BOA\client\bin",
+                @"D:\BOA\client\bin\en",
+                @"D:\BOA\HSM\server\bin",
+                @"D:\BOA\BOA.Integration\server\bin"
+            };
+            foreach (var searchDirectory in searchDirectories)
+            {
+                var filePath = $@"{searchDirectory}\{assemblyFileNameWithoutExtension}.dll";
+                if (File.Exists(filePath))
+                {
+                    trace($"Loading assembly: {filePath}");
+                    return Assembly.Load(File.ReadAllBytes(filePath));
+                }
+            }
+
+            trace($"Assembly Not Found: {assemblyFileNameWithoutExtension}");
+
+            return null;
+        }
+        
         /// <summary>
         ///     Finds the type.
         /// </summary>
@@ -53,7 +92,7 @@ namespace ApiInspector
 
                 var assemblyName = string.Join(".", destination);
 
-                return BoaAssemblyResolver.FindAssembly(assemblyName)?.GetType(fullName);
+                return FindAssembly(assemblyName)?.GetType(fullName);
             }
 
             return null;
