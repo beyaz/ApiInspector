65329974f5002f91abe8cde82122b663e48299be
Abdullah Beyaztaş <beyaz1404@gmail.com>
2022-12-09 23:36:40 +0300

    .

---
+++ ApiInspector/FileHelper.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+
+namespace ApiInspector;
+
+static class FileHelper
+{
+    static string WorkingDirectory
+    {
+        get
+        {
+            var myDocuments = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            var directoryPath = Path.Combine(myDocuments, "ApiInspector") + Path.DirectorySeparatorChar;
+            if (Directory.Exists(directoryPath))
+            {
+                Directory.CreateDirectory(directoryPath);
+            }
+
+            return directoryPath;
+        }
+    }
+
+    public static void WriteFail(Exception exception)
+    {
+        File.WriteAllText(WorkingDirectory + @"ApiInspector.Response.Error.json", exception.ToString());
+    }
+
+    public static void WriteSuccess(string responseAsJson)
+    {
+        File.WriteAllText(WorkingDirectory + @"ApiInspector.Response.json", responseAsJson);
+    }
+}
--- ApiInspector/Program.cs
+++ ApiInspector/Program.cs
@@ -1,5 +1,4 @@
-﻿using System.Diagnostics;
-using System.IO;
+﻿using System.IO;
 using System.Reflection;
 using Newtonsoft.Json;
 
@@ -7,111 +6,61 @@ namespace ApiInspector;
 
 internal class Program
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
         //args = new[] { @"GetMetadataNodes|d:\boa\server\bin\BOA.Types.ERP.PersonRelation.dll" };
         try
         {
             if (args == null)
-            { 
+            {
                 throw new Exception("CommandLine arguments cannot be null.");
-                
             }
 
             if (args.Length == 0)
             {
                 throw new Exception("CommandLine arguments cannot be empty.");
-                
             }
 
             var arr = (args[0] + string.Empty).Split('|');
             if (arr.Length != 2)
             {
                 throw new Exception($"CommandLine argument invalid. @argument: {arr[0]}");
-               
             }
 
             var methodName = arr[0];
             var parameter  = arr[1];
 
-            var methodInfo = typeof(Program).GetMethod(methodName, BindingFlags.Static| BindingFlags.Public| BindingFlags.NonPublic);
+            var methodInfo = typeof(Program).GetMethod(methodName, BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
             if (methodInfo == null)
             {
                 throw new Exception($"CommandLine argument invalid.Method not found. @methodName: {methodName}");
-                
             }
 
-        
             var response = methodInfo.Invoke(null, new object[] { parameter });
 
-            
             var responseAsJson = JsonConvert.SerializeObject(response, new JsonSerializerSettings
             {
-                Formatting = Formatting.Indented,
-                //TypeNameHandling = TypeNameHandling.All,
+                Formatting                 = Formatting.Indented,
                 PreserveReferencesHandling = PreserveReferencesHandling.Objects
             });
 
-            File.WriteAllText(@"c:\ApiInspector.Response.json", responseAsJson);
+            FileHelper.WriteSuccess(responseAsJson);
 
             Environment.Exit(1);
         }
         catch (Exception exception)
         {
-            File.WriteAllText(@"c:\ApiInspector.Response.Error.json", exception.ToString());
+            FileHelper.WriteFail(exception);
             Environment.Exit(0);
         }
     }
 
-    public static (IReadOnlyList<TypeReference> types, IReadOnlyList<MethodReference> methods) GetAssemblyModel(string assemblyFileFullPath)
+    internal static IEnumerable<MetadataNode> GetMetadataNodes(string assemblyFilePath)
     {
-        if (assemblyFileFullPath == null)
-        {
-            throw new ArgumentNullException(nameof(assemblyFileFullPath));
-        }
-
-        if (!File.Exists(assemblyFileFullPath))
-        {
-            throw new FileNotFoundException(assemblyFileFullPath);
-        }
-
-        AppDomain.CurrentDomain.AssemblyResolve+=(s,e)=>
-        {
-            if (File.Exists(@"d:\boa\server\bin\"+ e.Name))
-            {
-                return Assembly.LoadFile(@"d:\boa\server\bin\" + e.Name);
-            }
-
-            return null;
-        };
-
-        var assembly = Assembly.LoadFile(assemblyFileFullPath);
-
-        var types   = new List<TypeReference>();
-        var methods = new List<MethodReference>();
-
-        assembly.VisitTypes(t=>
-        {
-            types.Add(t.AsReference());
-            
-            t.VisitMethods(m=>methods.Add(m.AsReference()));
-        });
-
-        
-        
-        
-
-        return (types,methods);
-    }
-
-   
-
-    public static IEnumerable<MetadataNode> GetMetadataNodes(string assemblyFilePath)
-    {
-        AppDomain.CurrentDomain.AssemblyResolve += (s, e) =>
+        AppDomain.CurrentDomain.AssemblyResolve += (_, e) =>
         {
             var fileNameWithoutExtension = new AssemblyName(e.Name).Name;
-            
+
             if (File.Exists(@"d:\boa\server\bin\" + fileNameWithoutExtension + ".dll"))
             {
                 return Assembly.LoadFile(@"d:\boa\server\bin\" + fileNameWithoutExtension + ".dll");
@@ -119,8 +68,7 @@ internal class Program
 
             return null;
         };
-        
+
         return MetadataHelper.GetMetadataNodes(assemblyFilePath);
     }
-
 }
