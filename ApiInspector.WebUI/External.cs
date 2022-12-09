cf42235a2bd65686e39b0b243427768f5cf16192
Abdullah Beyaztaş <beyaz1404@gmail.com>
2022-12-09 13:57:46 +0300

    .

---
+++ ApiInspector.WebUI/External.cs
@@ -0,0 +1,106 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using Newtonsoft.Json;
+
+namespace ApiInspector.WebUI;
+
+static class External
+{
+    
+    
+    public static string GetAssemblyModel()
+    {
+        return Execute<string>(nameof(GetAssemblyModel), "abc").Unwrap();
+    }
+
+    public static TResponse Unwrap<TResponse>(this (TResponse response, Exception exception) tuple)
+    {
+        if (tuple.exception != null)
+        {
+            throw tuple.exception;
+        }
+
+        return tuple.response;
+    }
+    
+    public static (TResponse response, Exception exception) Execute<TResponse>(string methodName, string parameter)
+    {
+        var exitCode = RunProcess(methodName, parameter);
+        if (exitCode == 1) 
+        {
+            return (JsonConvert.DeserializeObject<TResponse>(ReadResponse()), null);
+        }
+
+        const string error = @"c:\ApiInspector.Response.Error.json";
+        if (File.Exists(error))
+        {
+            var errorMessage = File.ReadAllText(error);
+
+            File.Delete(error);
+
+            return (default, new Exception(errorMessage));
+        }
+
+        return (default, new Exception($"Unexpected exitCode: {exitCode}"));
+    }
+
+    public static string ReadResponse()
+    {
+        const string filePath = @"c:\ApiInspector.Response.json";
+
+        while (true)
+        {
+            if (File.Exists(filePath))
+            {
+                if (!IsFileLocked(filePath))
+                {
+                    var response = File.ReadAllText(filePath);
+                    
+                    File.Delete(filePath);
+
+                    return response;
+                }
+            }
+            
+            Thread.Sleep(100);
+        }
+
+        static bool IsFileLocked(string path)
+        {
+            FileStream stream = null;
+            try
+            {
+                FileInfo file = new FileInfo(path);
+                stream = file.Open(FileMode.Open, FileAccess.ReadWrite, FileShare.None);
+            }
+            catch (IOException)
+            {
+                return true;
+            }
+            finally
+            {
+                if (stream != null)
+                {
+                    stream.Close();
+                }
+            }
+            return false;
+        }
+    }
+    
+    
+
+    static int RunProcess(string methodName, string parameter)
+    {
+        Process process = new Process();
+        process.StartInfo.FileName  = @"D:\work\git\ApiInspector\ApiInspector\bin\Debug\ApiInspector.exe";
+        process.StartInfo.Arguments = $"{methodName}:{parameter}";
+        process.Start();
+        process.WaitForExit();
+        
+        return process.ExitCode;
+        
+        
+    }
+}
