fb8747d91ab217f3a0a19df9337168e46f2df8cc
Abdullah Beyaztaş <beyaz1404@gmail.com>
2022-12-17 01:45:09 +0300

    .

--- ApiInspector.WebUI/Config.cs
+++ ApiInspector.WebUI/Config.cs
@@ -6,6 +6,9 @@ namespace ApiInspector.WebUI;
 public class ConfigInfo
 {
     public string DotNetFrameworkInvokerExePath { get; set; }
+    public string DotNetCoreInvokerExePath { get; set; }
+
+    
     public string BrowserExePath { get; set; }
     public string BrowserExeArguments { get; set; }
     public bool HideConsoleWindow { get; set; }
--- ApiInspector.WebUI/External.cs
+++ ApiInspector.WebUI/External.cs
@@ -6,30 +6,38 @@ namespace ApiInspector.WebUI;
 
 static class External
 {
-    public static (string jsonForInstance, string jsonForParameters) GetEditorTexts(string fullAssemblyPath, MethodReference methodReference, string jsonForInstance, string jsonForParameters)
+    public static (string jsonForInstance, string jsonForParameters) GetEditorTexts(string assemblyFileFullPath, MethodReference methodReference, string jsonForInstance, string jsonForParameters)
     {
-        return Execute<(string jsonForInstance, string jsonForParameters)>(nameof(GetEditorTexts), (fullAssemblyPath, methodReference, jsonForInstance, jsonForParameters)).Unwrap();
+        var isDotNetCoreAssembly = IsDotNetCoreAssembly(assemblyFileFullPath);
+
+        var parameter = (assemblyFileFullPath, methodReference, jsonForInstance, jsonForParameters);
+
+        return Execute<(string jsonForInstance, string jsonForParameters)>(isDotNetCoreAssembly, nameof(GetEditorTexts), parameter).Unwrap();
     }
 
-    public static IEnumerable<MetadataNode> GetMetadataNodes(string assemblyFilePath, string classFilter, string methodFilter)
+    public static IEnumerable<MetadataNode> GetMetadataNodes(string assemblyFileFullPath, string classFilter, string methodFilter)
     {
-        if (GetTargetFramework(new FileInfo(assemblyFilePath)).isDotNetFramework)
-        {
-            return Execute<IEnumerable<MetadataNode>>(nameof(GetMetadataNodes), (assemblyFilePath, classFilter, methodFilter)).Unwrap();
-        }
-        return Execute<IEnumerable<MetadataNode>>(nameof(GetMetadataNodes), (assemblyFilePath, classFilter, methodFilter)).Unwrap();
+        var isDotNetCoreAssembly = IsDotNetCoreAssembly(assemblyFileFullPath);
+
+        var parameter = (assemblyFileFullPath, classFilter, methodFilter);
+
+        return Execute<IEnumerable<MetadataNode>>(isDotNetCoreAssembly, nameof(GetMetadataNodes), parameter).Unwrap();
     }
 
-    public static string InvokeMethod(string fullAssemblyPath, MethodReference methodReference, string stateJsonTextForDotNetInstanceProperties, string stateJsonTextForDotNetMethodParameters, bool waitForDebugger)
+    public static string InvokeMethod(string assemblyFileFullPath, MethodReference methodReference, string stateJsonTextForDotNetInstanceProperties, string stateJsonTextForDotNetMethodParameters, bool waitForDebugger)
     {
-        return Execute<string>(nameof(InvokeMethod), (fullAssemblyPath, methodReference, stateJsonTextForDotNetInstanceProperties, stateJsonTextForDotNetMethodParameters), waitForDebugger).Unwrap();
+        var isDotNetCoreAssembly = IsDotNetCoreAssembly(assemblyFileFullPath);
+
+        var parameter = (assemblyFileFullPath, methodReference, stateJsonTextForDotNetInstanceProperties, stateJsonTextForDotNetMethodParameters);
+
+        return Execute<string>(isDotNetCoreAssembly, nameof(InvokeMethod), parameter, waitForDebugger).Unwrap();
     }
 
-    static (TResponse response, Exception exception) Execute<TResponse>(string methodName, object parameter, bool waitForDebugger = false)
+    static (TResponse response, Exception exception) Execute<TResponse>(bool runCoreApp, string methodName, object parameter, bool waitForDebugger = false)
     {
-        FileHelper.WriteInput(JsonConvert.SerializeObject(parameter,new JsonSerializerSettings{Formatting = Formatting.Indented,DefaultValueHandling = DefaultValueHandling.Ignore}));
+        FileHelper.WriteInput(JsonConvert.SerializeObject(parameter, new JsonSerializerSettings { Formatting = Formatting.Indented, DefaultValueHandling = DefaultValueHandling.Ignore }));
 
-        var exitCode = RunProcess(methodName, waitForDebugger);
+        var exitCode = RunProcess(runCoreApp, methodName, waitForDebugger);
         if (exitCode == 1)
         {
             return (JsonConvert.DeserializeObject<TResponse>(FileHelper.ReadResponse()), null);
@@ -43,10 +51,15 @@ static class External
         return (default, new Exception($"Unexpected exitCode: {exitCode}"));
     }
 
-    static int RunProcess(string methodName, bool waitForDebugger)
+    static bool IsDotNetCoreAssembly(string assemblyFileFullPath)
+    {
+        return GetTargetFramework(new FileInfo(assemblyFileFullPath)).isDotNetCore;
+    }
+
+    static int RunProcess(bool runCoreApp, string methodName, bool waitForDebugger)
     {
         var process = new Process();
-        process.StartInfo.FileName  = Config.DotNetFrameworkInvokerExePath;
+        process.StartInfo.FileName  = runCoreApp ? Config.DotNetCoreInvokerExePath : Config.DotNetFrameworkInvokerExePath;
         process.StartInfo.Arguments = $"{(waitForDebugger ? "1" : "0")}|{methodName}";
         process.Start();
         process.WaitForExit();
