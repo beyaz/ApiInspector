8d8fc3c0f2c315938524525a6af2672512dc7267
Abdullah Beyaztaş <beyaz1404@gmail.com>
2022-12-17 19:26:08 +0300

    .

--- ApiInspector.Bootstrapper/Program.cs
+++ ApiInspector.Bootstrapper/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
 
 namespace ApiInspector.Bootstrapper
 {
@@ -6,7 +9,31 @@ namespace ApiInspector.Bootstrapper
     {
         static void Main()
         {
-           
+            
+
+
+            Console.WriteLine("Checking version...");
+
+            using var client = new HttpClient();
+
+
+            var versionUrl = System.AppContext.GetData("VersionUrl") as string;
+
+            var newVersionZipFileUrl = System.AppContext.GetData("NewVersionZipFileUrl") as string;
+
+            var installationFolder = System.AppContext.GetData("InstallationFolder") as string;
+
+            var webClient = new WebClient();
+            
+            if (!Directory.Exists(installationFolder))
+            {
+                
+                webClient.DownloadFile(new Uri(newVersionZipFileUrl), "d:\\r.zip");
+                
+                return;
+            }
+            var remoteVersion = webClient.DownloadString(versionUrl);
+            
 
             
         }
