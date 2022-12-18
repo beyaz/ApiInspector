ff779b1a7eb9fa312a7e4fc3a003461aa9459f96
Abdullah Beyaztaş <beyaz1404@gmail.com>
2022-12-18 09:09:21 +0300

    .

--- ApiInspector.WebUI/wwwroot/index.jsx
+++ ApiInspector.WebUI/wwwroot/index.jsx
@@ -1,40 +1,7 @@
 // import core library
-import ReactWithDotNet from "./react-with-dotnet/react-with-dotnet";
+// import ReactWithDotNet from "./react-with-dotnet/react-with-dotnet";
 
 // import libraries which you use in your porject
 import "./react-with-dotnet/libraries/uiw-react-codemirror";
 import "./react-with-dotnet/libraries/react-free-scrollbar";
 import "./react-with-dotnet/libraries/primereact";
-
-var currentScrollY = 0;
-
-document.addEventListener('scroll', () => 
-{
-    var scrollY = window.scrollY;
-
-    function canFireAction()
-    {
-        if (scrollY > 0)
-        {
-            return currentScrollY === 0;
-        }
-
-        if (currentScrollY > 0)
-        {
-            return true;
-        }
-
-        return false;
-    }
-
-    if (canFireAction())
-    {
-        currentScrollY = scrollY;
-
-        ReactWithDotNet.DispatchEvent('MainContentDivScrollChangedOverZero', [scrollY]);
-    }
-    else
-    {
-        currentScrollY = scrollY;
-    }
-});
