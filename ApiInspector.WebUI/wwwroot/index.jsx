0c42fd8dbf8ec4b3847d2cadbfad6c01c3884563
Abdullah Beyaztaş <beyaz1404@gmail.com>
2022-12-18 08:39:57 +0300

    .

--- ApiInspector.WebUI/wwwroot/index.jsx
+++ ApiInspector.WebUI/wwwroot/index.jsx
@@ -2,7 +2,7 @@
 import ReactWithDotNet from "./react-with-dotnet/react-with-dotnet";
 
 // import libraries which you use in your porject
-import "./react-with-dotnet/libraries/react-xarrows";
+import "./react-with-dotnet/libraries/uiw-react-codemirror";
 import "./react-with-dotnet/libraries/react-free-scrollbar";
 import "./react-with-dotnet/libraries/primereact";
 
