a560b92a5d4e98958d13f8df476842a83e24454d
Abdullah Beyaztaş <beyaz1404@gmail.com>
2022-12-19 10:01:55 +0300

    .

--- ApiInspector.Bootstrapper/Program.cs
+++ ApiInspector.Bootstrapper/Program.cs
@@ -11,100 +11,103 @@ namespace ApiInspector.Bootstrapper
     {
         static void Main()
         {
-            Console.WriteLine("Checking version...");
-
-            using var client = new HttpClient();
-
-            var versionUrl = (string)AppContext.GetData("VersionUrl");
-            if (versionUrl == null)
+            try
             {
-                Console.WriteLine($"Value @{nameof(versionUrl)} cannot be empty.");
-                Console.Read();
-                return;
-            }
+                Console.WriteLine("Checking version...");
 
-            var newVersionZipFileUrl = (string)AppContext.GetData("NewVersionZipFileUrl");
-            if (newVersionZipFileUrl == null)
-            {
-                Console.WriteLine($"Value @{nameof(newVersionZipFileUrl)} cannot be empty.");
-                Console.Read();
-                return;
-            }
+                using var client = new HttpClient();
 
-            var installationFolder = (string)AppContext.GetData("InstallationFolder");
-            if (installationFolder == null)
-            {
-                Console.WriteLine($"Value @{nameof(installationFolder)} cannot be empty.");
-                Console.Read();
-                return;
-            }
-
-            installationFolder = installationFolder.Replace("{MyDocuments}", Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments));
-
-
-            var appFolder = Path.Combine(installationFolder, "Api Inspector (.net method invoker)");
-
-            var webClient = new WebClient();
-
-            var shouldUpdate = true;
-
-            var localVersionFilePath = Path.Combine(appFolder, "Version.txt");
-
-            if (File.Exists(localVersionFilePath))
-            {
-                var remoteVersion = int.Parse(webClient.DownloadString(versionUrl));
-                var localVersion  = int.Parse(File.ReadAllText(localVersionFilePath));
-                if (remoteVersion == localVersion)
+                var versionUrl = (string)AppContext.GetData("VersionUrl");
+                if (versionUrl == null)
                 {
-                    shouldUpdate = false;
-                }
-            }
-
-            if (shouldUpdate)
-            {
-                Console.WriteLine("Updating to new version...");
-
-                if (!Directory.Exists(appFolder))
-                {
-                    Directory.CreateDirectory(appFolder);
+                    throw new Exception($"Value @{nameof(versionUrl)} cannot be empty.");
                 }
 
-                var localZipFilePath = Path.Combine(installationFolder, "Remote.zip");
-
-                webClient.DownloadFile(new Uri(newVersionZipFileUrl), localZipFilePath);
-
-                ZipFile.ExtractToDirectory(localZipFilePath, installationFolder, true);
-
-                File.Delete(localZipFilePath);
-
-                var remoteVersion = int.Parse(webClient.DownloadString(versionUrl));
-                File.WriteAllText(localVersionFilePath, remoteVersion.ToString());
-            }
-
-            // copy plugins
-            {
-                var directory = Path.GetDirectoryName(typeof(Program).Assembly.Location);
-                if (!string.IsNullOrWhiteSpace(directory))
+                var newVersionZipFileUrl = (string)AppContext.GetData("NewVersionZipFileUrl");
+                if (newVersionZipFileUrl == null)
                 {
-                    foreach (var file in Directory.GetFiles(directory, "*.json"))
+                    throw new Exception($"Value @{nameof(newVersionZipFileUrl)} cannot be empty.");
+                }
+
+                var installationFolder = (string)AppContext.GetData("InstallationFolder");
+                if (installationFolder == null)
+                {
+                    throw new Exception($"Value @{nameof(installationFolder)} cannot be empty.");
+                }
+
+                installationFolder = installationFolder.Replace("{MyDocuments}", Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments));
+
+                var appFolder = Path.Combine(installationFolder, "Api Inspector (.net method invoker)");
+
+                var webClient = new WebClient();
+
+                var shouldUpdate = true;
+
+                Console.WriteLine("AppFolder: " + appFolder);
+
+                var localVersionFilePath = Path.Combine(appFolder, "Version.txt");
+
+                if (File.Exists(localVersionFilePath))
+                {
+                    var remoteVersion = int.Parse(webClient.DownloadString(versionUrl));
+                    var localVersion  = int.Parse(File.ReadAllText(localVersionFilePath));
+
+                    Console.WriteLine($"Remote Version: {remoteVersion}");
+                    Console.WriteLine($"Local Version: {localVersion}");
+
+                    if (remoteVersion == localVersion)
                     {
-                        if (Path.GetFileNameWithoutExtension(file).StartsWith("ApiInspector.Plugin.", StringComparison.OrdinalIgnoreCase))
+                        shouldUpdate = false;
+                    }
+                }
+
+                if (shouldUpdate)
+                {
+                    Console.WriteLine("Updating to new version...");
+
+                    if (!Directory.Exists(appFolder))
+                    {
+                        Directory.CreateDirectory(appFolder);
+                    }
+
+                    var localZipFilePath = Path.Combine(installationFolder, "Remote.zip");
+
+                    Console.WriteLine($"Downloading... {newVersionZipFileUrl}");
+
+                    webClient.DownloadFile(new Uri(newVersionZipFileUrl), localZipFilePath);
+
+                    Console.WriteLine("Extracting...");
+
+                    ZipFile.ExtractToDirectory(localZipFilePath, installationFolder, true);
+
+                    File.Delete(localZipFilePath);
+
+                    var remoteVersion = int.Parse(webClient.DownloadString(versionUrl));
+                    File.WriteAllText(localVersionFilePath, remoteVersion.ToString());
+                }
+
+                // copy plugins
+                {
+                    var directory = Path.GetDirectoryName(typeof(Program).Assembly.Location);
+                    if (!string.IsNullOrWhiteSpace(directory))
+                    {
+                        foreach (var file in Directory.GetFiles(directory, "*.json"))
                         {
-                            if (file.IndexOf("NetFramework", StringComparison.OrdinalIgnoreCase) > 0)
+                            if (Path.GetFileNameWithoutExtension(file).StartsWith("ApiInspector.Plugin.", StringComparison.OrdinalIgnoreCase))
                             {
-                                File.Copy(file, Path.Combine(appFolder, "ApiInspector.NetFramework", Path.GetFileName(file)), true);
-                            }
-                            else
-                            {
-                                File.Copy(file, Path.Combine(appFolder, "ApiInspector.NetCore", Path.GetFileName(file)), true);
+                                if (file.IndexOf("NetFramework", StringComparison.OrdinalIgnoreCase) > 0)
+                                {
+                                    File.Copy(file, Path.Combine(appFolder, "ApiInspector.NetFramework", Path.GetFileName(file)), true);
+                                }
+                                else
+                                {
+                                    File.Copy(file, Path.Combine(appFolder, "ApiInspector.NetCore", Path.GetFileName(file)), true);
+                                }
                             }
                         }
                     }
                 }
-            }
 
-            try
-            {
                 Console.WriteLine("Starting...");
                 Process.Start(Path.Combine(appFolder, "ApiInspector.WebUI.exe"));
             }
