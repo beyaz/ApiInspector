d8e6a1f877ed0283b65b6b83a82260216278a470
beyaz <beyaz1404@gmail.com>
2023-09-23 02:00:00 +0300

    .

---
+++ ApiInspector.WebUI/FileStorage.cs
@@ -0,0 +1,88 @@
+﻿using System.IO;
+
+namespace ApiInspector.WebUI;
+
+static class FileStorage
+{
+    static readonly string CacheDirectoryPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), nameof(ApiInspector)) +
+                                                Path.DirectorySeparatorChar +
+                                                "Cache" +
+                                                Path.DirectorySeparatorChar;
+
+    public static void DeleteFromStorage(string storageKey)
+    {
+        var filePath = ToFilePath(storageKey);
+
+        if (File.Exists(filePath))
+        {
+            File.Delete(filePath);
+        }
+    }
+
+    public static bool ExistInStorage(string storageKey)
+    {
+        return File.Exists(ToFilePath(storageKey));
+    }
+
+    public static string ReadFromStorage(string storageKey)
+    {
+        return File.ReadAllText(ToFilePath(storageKey));
+    }
+
+    public static void SaveToStorage(string storageKey, string storageValue)
+    {
+        WriteAllText(ToFilePath(storageKey), storageValue);
+    }
+
+    public static IReadOnlyList<(string StorageKey, string StorageValue)> SearchInStoreage(string filter, int topN)
+    {
+        return SearchInStoreage(filter).Take(topN).ToList();
+    }
+
+    static IEnumerable<(string StorageKey, string StorageValue)> SearchInStoreage(string filter)
+    {
+        var cacheDirectoryPath = CacheDirectoryPath;
+
+        if (!Directory.Exists(cacheDirectoryPath))
+        {
+            yield break;
+        }
+
+        foreach (var directory in Directory.GetDirectories(cacheDirectoryPath).OrderByDescending(x => new DirectoryInfo(x).LastWriteTime))
+        {
+            foreach (var filePath in Directory.GetFiles(directory).OrderByDescending(x => new FileInfo(x).LastWriteTime))
+            {
+                var fileContent = File.ReadAllText(filePath);
+
+                if (fileContent.IndexOf(filter + string.Empty, StringComparison.OrdinalIgnoreCase) >= 0)
+                {
+                    yield return (ToStorageKey(filePath), fileContent);
+                }
+            }
+        }
+    }
+
+    static string ToFilePath(string storageKey)
+    {
+        return Path.Combine(CacheDirectoryPath, storageKey);
+    }
+
+    static string ToStorageKey(string filePath)
+    {
+        return filePath.RemoveFromStart(CacheDirectoryPath);
+    }
+
+    static void WriteAllText(string path, string contents)
+    {
+        var directoryName = Path.GetDirectoryName(path);
+        if (directoryName != null)
+        {
+            if (!Directory.Exists(directoryName))
+            {
+                Directory.CreateDirectory(directoryName);
+            }
+        }
+
+        File.WriteAllText(path, contents);
+    }
+}
--- ApiInspector.WebUI/Storage.cs
+++ ApiInspector.WebUI/Storage.cs
@@ -1,88 +1,29 @@
-﻿using System.IO;
-
-namespace ApiInspector.WebUI;
+﻿namespace ApiInspector.WebUI;
 
 static class Storage
 {
-    static readonly string CacheDirectoryPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), nameof(ApiInspector)) +
-                                                Path.DirectorySeparatorChar +
-                                                "Cache" +
-                                                Path.DirectorySeparatorChar;
-
     public static void DeleteFromStorage(string storageKey)
     {
-        var filePath = ToFilePath(storageKey);
-
-        if (File.Exists(filePath))
-        {
-            File.Delete(filePath);
-        }
+        FileStorage.DeleteFromStorage(storageKey);
     }
 
     public static bool ExistInStorage(string storageKey)
     {
-        return File.Exists(ToFilePath(storageKey));
+        return FileStorage.ExistInStorage(storageKey);
     }
 
     public static string ReadFromStorage(string storageKey)
     {
-        return File.ReadAllText(ToFilePath(storageKey));
+        return FileStorage.ReadFromStorage(storageKey);
     }
 
     public static void SaveToStorage(string storageKey, string storageValue)
     {
-        WriteAllText(ToFilePath(storageKey), storageValue);
+        FileStorage.SaveToStorage(storageKey, storageValue);
     }
 
     public static IReadOnlyList<(string StorageKey, string StorageValue)> SearchInStoreage(string filter, int topN)
     {
-        return SearchInStoreage(filter).Take(topN).ToList();
-    }
-
-    static IEnumerable<(string StorageKey, string StorageValue)> SearchInStoreage(string filter)
-    {
-        var cacheDirectoryPath = CacheDirectoryPath;
-
-        if (!Directory.Exists(cacheDirectoryPath))
-        {
-            yield break;
-        }
-
-        foreach (var directory in Directory.GetDirectories(cacheDirectoryPath).OrderByDescending(x => new DirectoryInfo(x).LastWriteTime))
-        {
-            foreach (var filePath in Directory.GetFiles(directory).OrderByDescending(x => new FileInfo(x).LastWriteTime))
-            {
-                var fileContent = File.ReadAllText(filePath);
-
-                if (fileContent.IndexOf(filter + string.Empty, StringComparison.OrdinalIgnoreCase) >= 0)
-                {
-                    yield return (ToStorageKey(filePath), fileContent);
-                }
-            }
-        }
-    }
-
-    static string ToFilePath(string storageKey)
-    {
-        return Path.Combine(CacheDirectoryPath, storageKey);
-    }
-
-    static string ToStorageKey(string filePath)
-    {
-        return filePath.RemoveFromStart(CacheDirectoryPath);
-    }
-
-    static void WriteAllText(string path, string contents)
-    {
-        var directoryName = Path.GetDirectoryName(path);
-        if (directoryName != null)
-        {
-            if (!Directory.Exists(directoryName))
-            {
-                Directory.CreateDirectory(directoryName);
-            }
-        }
-
-        File.WriteAllText(path, contents);
+        return FileStorage.SearchInStoreage(filter, topN);
     }
 }
