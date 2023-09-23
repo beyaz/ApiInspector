f1731f30e5daa3e4c262cb098faf1488dbd73343
beyaz <beyaz1404@gmail.com>
2023-09-23 03:03:35 +0300

    .

---
+++ ApiInspector.WebUI/DbStorage.cs
@@ -0,0 +1,23 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using Dapper;
+
+namespace ApiInspector.WebUI;
+
+static class DbStorage
+{
+    public static void Delete()
+    {
+        using (var sqlConnection = new SqlConnection(Config.DbStorage.ConnectionString))
+        {
+            if (sqlConnection.State != ConnectionState.Open)
+            {
+                sqlConnection.Open();
+            }
+
+            sqlConnection.ExecuteScalar("");
+
+
+        }
+    }
+}
