638bc0d27c978129934011405dabf0356ed38aff
beyaz <beyaz1404@gmail.com>
2023-10-05 12:41:56 +0300

    .

--- ApiInspector.Bootstrapper/Program.cs
+++ ApiInspector.Bootstrapper/Program.cs
@@ -9,12 +9,17 @@ namespace ApiInspector.Bootstrapper;
 
 static class Program
 {
-
-    class Config
+    public static async Task Main()
     {
-        public string VersionUrl { get; init; }
-        public string NewVersionZipFileUrl { get; init; }
-        public string InstallationFolder { get; init; }
+        try
+        {
+            await Run(CalculateConfig());
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine(exception);
+            Console.Read();
+        }
     }
 
     static Config CalculateConfig()
@@ -43,95 +48,6 @@ static class Program
             NewVersionZipFileUrl = newVersionZipFileUrl,
             InstallationFolder   = installationFolder
         };
-    }
-    
-    public static async Task Main()
-    {
-        try
-        {
-            await Run(CalculateConfig());
-        }
-        catch (Exception exception)
-        {
-            Console.WriteLine(exception);
-            Console.Read();
-        }
-    }
-
-    static async Task Run(Config config)
-    {
-        KillAllNamedProcess("ApiInspector.WebUI");
-        KillAllNamedProcess("ApiInspector");
-
-        Console.WriteLine("Checking version...");
-
-        using var client = new HttpClient();
-
-        var versionUrl = config.VersionUrl;
-        var newVersionZipFileUrl = config.NewVersionZipFileUrl;
-        var installationFolder = config.InstallationFolder;
-
-        installationFolder = installationFolder.Replace("{MyDocuments}", Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments));
-
-        var appFolder = Path.Combine(installationFolder, "Api Inspector (.net method invoker)");
-
-        var shouldUpdate = true;
-
-        Console.WriteLine("AppFolder: " + appFolder);
-
-        var localVersionFilePath = Path.Combine(appFolder, "Version.txt");
-
-        if (File.Exists(localVersionFilePath))
-        {
-            var remoteVersion = await DownloadStringAsync(versionUrl).Then(int.Parse);
-
-            var localVersion = await File.ReadAllTextAsync(localVersionFilePath).Then(int.Parse);
-
-            Console.WriteLine($"Remote Version: {remoteVersion}");
-            Console.WriteLine($"Local Version : {localVersion}");
-
-            if (remoteVersion <= localVersion)
-            {
-                shouldUpdate = false;
-            }
-        }
-
-        if (shouldUpdate)
-        {
-            Console.WriteLine("Updating to new version...");
-
-            if (Directory.Exists(appFolder))
-            {
-                Directory.Delete(appFolder);
-            }
-
-            Directory.CreateDirectory(appFolder);
-
-            var localZipFilePath = Path.Combine(installationFolder, "Remote.zip");
-            if (File.Exists(localZipFilePath))
-            {
-                Console.WriteLine("Clearing zip file...");
-                File.Delete(localZipFilePath);
-            }
-
-            Console.WriteLine($"Downloading... {newVersionZipFileUrl}");
-
-            await DownloadFileAsync(newVersionZipFileUrl, localZipFilePath);
-
-            Console.WriteLine("Extracting...");
-
-            ZipFile.ExtractToDirectory(localZipFilePath, installationFolder, true);
-
-            File.Delete(localZipFilePath);
-
-            var remoteVersion = await DownloadStringAsync(versionUrl).Then(int.Parse);
-
-            await File.WriteAllTextAsync(localVersionFilePath, remoteVersion.ToString());
-        }
-
-        CopyPlugins(appFolder);
-
-        StartWebApplication(appFolder);
     }
 
     static void CopyPlugins(string appFolder)
@@ -197,15 +113,102 @@ static class Program
         }
     }
 
+    static async Task Run(Config config)
+    {
+        Action<string> trace = Console.WriteLine;
+
+        KillAllNamedProcess("ApiInspector.WebUI");
+        KillAllNamedProcess("ApiInspector");
+
+        trace("Checking version...");
+
+        using var client = new HttpClient();
+
+        var versionUrl = config.VersionUrl;
+        var newVersionZipFileUrl = config.NewVersionZipFileUrl;
+        var installationFolder = config.InstallationFolder;
+
+        installationFolder = installationFolder.Replace("{MyDocuments}", Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments));
+
+        var appFolder = Path.Combine(installationFolder, "Api Inspector (.net method invoker)");
+
+        var shouldUpdate = true;
+
+        trace("AppFolder: " + appFolder);
+
+        var localVersionFilePath = Path.Combine(appFolder, "Version.txt");
+
+        if (File.Exists(localVersionFilePath))
+        {
+            var remoteVersion = await DownloadStringAsync(versionUrl).Then(int.Parse);
+
+            var localVersion = await File.ReadAllTextAsync(localVersionFilePath).Then(int.Parse);
+
+            trace($"Remote Version: {remoteVersion}");
+            trace($"Local Version : {localVersion}");
+
+            if (remoteVersion <= localVersion)
+            {
+                shouldUpdate = false;
+            }
+        }
+
+        if (shouldUpdate)
+        {
+            trace("Updating to new version...");
+
+            if (Directory.Exists(appFolder))
+            {
+                trace($"Deleting previous version...{appFolder}");
+                var isDeleted = await TryDeleteDirectory(appFolder);
+                if (!isDeleted)
+                {
+                    trace($"Please remove folder manually. {appFolder}");
+                    Console.Read();
+                    return;
+                }
+
+                trace("Previous version successfully removed.");
+            }
+
+            Directory.CreateDirectory(appFolder);
+
+            var localZipFilePath = Path.Combine(installationFolder, "Remote.zip");
+            if (File.Exists(localZipFilePath))
+            {
+                trace("Clearing zip file...");
+                File.Delete(localZipFilePath);
+            }
+
+            trace($"Downloading... {newVersionZipFileUrl}");
+
+            await DownloadFileAsync(newVersionZipFileUrl, localZipFilePath);
+
+            trace("Extracting...");
+
+            ZipFile.ExtractToDirectory(localZipFilePath, installationFolder, true);
+
+            File.Delete(localZipFilePath);
+
+            var remoteVersion = await DownloadStringAsync(versionUrl).Then(int.Parse);
+
+            await File.WriteAllTextAsync(localVersionFilePath, remoteVersion.ToString());
+        }
+
+        CopyPlugins(appFolder);
+
+        StartWebApplication(appFolder);
+    }
+
     static void StartWebApplication(string appFolder)
     {
         Console.WriteLine("Starting...");
-        
+
         var process = Process.Start(Path.Combine(appFolder, "ApiInspector.WebUI.exe"));
-        
+
         if (process?.HasExited == true)
         {
-            Console.WriteLine($"Exieted with exitCode: {process.ExitCode}"); 
+            Console.WriteLine($"Exieted with exitCode: {process.ExitCode}");
         }
         else
         {
@@ -218,5 +221,45 @@ static class Program
         var a = await task;
 
         return nextFunc(a);
+    }
+
+    static async Task<bool> TryDeleteDirectory(string directoryPath, int maxRetries = 10, int millisecondsDelay = 30)
+    {
+        if (directoryPath == null)
+            throw new ArgumentNullException(nameof(directoryPath));
+        if (maxRetries < 1)
+            throw new ArgumentOutOfRangeException(nameof(maxRetries));
+        if (millisecondsDelay < 1)
+            throw new ArgumentOutOfRangeException(nameof(millisecondsDelay));
+
+        for (var i = 0; i < maxRetries; ++i)
+        {
+            try
+            {
+                if (Directory.Exists(directoryPath))
+                {
+                    Directory.Delete(directoryPath, true);
+                }
+
+                return true;
+            }
+            catch (IOException)
+            {
+                await Task.Delay(millisecondsDelay);
+            }
+            catch (UnauthorizedAccessException)
+            {
+                await Task.Delay(millisecondsDelay);
+            }
+        }
+
+        return false;
+    }
+
+    class Config
+    {
+        public string InstallationFolder { get; init; }
+        public string NewVersionZipFileUrl { get; init; }
+        public string VersionUrl { get; init; }
     }
 }
