366e682230327a5e1f93c1f4085005ac3bca100d
beyaz <beyaz1404@gmail.com>
2024-05-17 13:45:38 +0300

    .

--- ApiInspector.WebUI/wwwroot/index.jsx
+++ ApiInspector.WebUI/wwwroot/index.jsx
@@ -4,7 +4,6 @@ import ReactWithDotNet from "./react-with-dotnet/react-with-dotnet";
 // you can comment these imports according to your project dependency
 
 import "./react-with-dotnet/libraries/rsuite/all";
-import "./react-with-dotnet/libraries/primereact/all";
 import "./react-with-dotnet/libraries/react-free-scrollbar/all";
 import "./react-with-dotnet/libraries/MonacoEditorReact/all";
 import "./react-with-dotnet/libraries/mui-core/all";
