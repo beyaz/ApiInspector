a03246edc3869edcbea06d192b590edd468c09b7
beyaz <beyaz1404@gmail.com>
2024-05-17 12:39:08 +0300

    .

--- ApiInspector.WebUI/wwwroot/index.jsx
+++ ApiInspector.WebUI/wwwroot/index.jsx
@@ -8,6 +8,7 @@ import "./react-with-dotnet/libraries/primereact/all";
 import "./react-with-dotnet/libraries/react-free-scrollbar/all";
 import "./react-with-dotnet/libraries/MonacoEditorReact/all";
 import "./react-with-dotnet/libraries/mui-core/all";
+import "./react-with-dotnet/libraries/react-split/all";
 
 ReactWithDotNet.RegisterExternalJsObject("CloseWindow", () => window.close());
 
