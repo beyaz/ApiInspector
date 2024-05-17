f12a549dd833639205d88d4574418116b0a5bda6
beyaz <beyaz1404@gmail.com>
2024-05-17 15:05:16 +0300

    .

--- ApiInspector.WebUI/Components/TwoRowSplittedForm.cs
+++ ApiInspector.WebUI/Components/TwoRowSplittedForm.cs
@@ -6,6 +6,39 @@ sealed class TwoRowSplittedForm : PureComponent
 {
     public int[] sizes { get; init; } = [50, 50];
 
+    public static Element Create(Element[] children, int[] sizes)
+    {
+        return new FlexRow(SizeFull)
+        {
+            new style
+            {
+                new CssClass("gutter",
+                [
+                    PaddingLeftRight(8),
+                    BackgroundRepeatNoRepeat,
+                    BackgroundPosition("50%")
+                ]),
+                new CssClass("gutter.gutter-horizontal",
+                [
+                    BackgroundImage("url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAeCAYAAADkftS9AAAAIklEQVQoU2M4c+bMfxAGAgYYmwGrIIiDjrELjpo5aiZeMwF+yNnOs5KSvgAAAABJRU5ErkJggg==')"),
+                    Cursor("col-resize")
+                ])
+            },
+
+            new Split
+            {
+                sizes      = sizes,
+                gutterSize = 12,
+                style      = { SizeFull, DisplayFlexRow },
+
+                children =
+                {
+                    children
+                }
+            }
+        };
+    }
+
     protected override Element render()
     {
         return new FlexRow(SizeFull)
