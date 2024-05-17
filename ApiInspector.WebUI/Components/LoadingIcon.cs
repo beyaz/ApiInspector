f67c316bbf00f20644b5c548ee105b2f0ad47ec4
beyaz <beyaz1404@gmail.com>
2024-05-17 17:29:23 +0300

    .

--- ApiInspector.WebUI/Components/LoadingIcon.cs
+++ ApiInspector.WebUI/Components/LoadingIcon.cs
@@ -30,7 +30,17 @@ public class LoadingIcon : PureComponent
                   0% { transform: rotate(0deg); }
                   100% { transform: rotate(360deg); }
                 }
-                """
+                """,
+
+                new CssClass("loader",
+                [
+                    Border(1, solid, "#f3f3f3"),
+                    BorderRadius("50%"),
+                    BorderTop("#3498db"),
+                    SizeFull,
+                    WebkitAnimation("spin 1s linear infinite"),
+                    Animation("spin 1s linear infinite")
+                ])
             },
 
             new div { className = "loader", style = { Size("100%") } }
