e9391112ffe2ee89b6110a0ff6648b5bd3cf97c1
beyaz <beyaz1404@gmail.com>
2024-05-18 10:34:48 +0300

    .

--- ApiInspector.NetFramework/MetaDataHelper.cs
+++ ApiInspector.NetFramework/MetaDataHelper.cs
@@ -2,23 +2,23 @@
 
 namespace ApiInspector;
 
-class MetadataNode
+sealed class MetadataNode
 {
     public List<MetadataNode> children { get; set; }
 
-    public bool IsClass { get; set; }
+    public bool IsClass { get; init; }
 
-    public bool IsMethod { get; set; }
+    public bool IsMethod { get; init; }
 
-    public bool IsNamespace { get; set; }
+    public bool IsNamespace { get; init; }
 
     public string label { get; set; }
 
-    public MethodReference MethodReference { get; set; }
+    public MethodReference MethodReference { get; init; }
 
-    public string NamespaceReference { get; set; }
+    public string NamespaceReference { get; init; }
 
-    public TypeReference TypeReference { get; set; }
+    public TypeReference TypeReference { get; init; }
 }
 
 static class MetadataHelper
