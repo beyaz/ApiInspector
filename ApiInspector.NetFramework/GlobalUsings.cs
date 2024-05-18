0f3d10bf955f71c0c695be38e81f9ce0122e6b22
beyaz <beyaz1404@gmail.com>
2024-05-18 10:30:59 +0300

    .

--- ApiInspector.NetFramework/GlobalUsings.cs
+++ ApiInspector.NetFramework/GlobalUsings.cs
@@ -1,3 +1,8 @@
 ﻿global using System;
 global using System.Collections.Generic;
 global using System.Linq;
+
+namespace System.Runtime.CompilerServices
+{
+    internal static class IsExternalInit {}
+}
