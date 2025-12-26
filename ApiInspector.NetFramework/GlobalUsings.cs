5a52bf998bca9ef0882c890b96c10eb925b2547c
Abdullah Beyaztaş <beyaz1404@gmail.com>
2025-12-27 00:13:37 +0300

    .

--- ApiInspector.NetFramework/GlobalUsings.cs
+++ ApiInspector.NetFramework/GlobalUsings.cs
@@ -1,7 +1,6 @@
 ﻿global using System;
 global using System.Collections.Generic;
 global using System.Linq;
-global using static ApiInspector.FileHelper;
 
 namespace System.Runtime.CompilerServices
 {
