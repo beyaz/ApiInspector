8889b42dae262d91d286aeeec2323486d66f571c
Abdullah Beyaztaş <beyaz1404@gmail.com>
2025-12-27 22:51:44 +0300

    .

--- ApiInspector.WebUI/MainWindow.cs
+++ ApiInspector.WebUI/MainWindow.cs
@@ -546,7 +546,7 @@ class MainWindow : Component<MainWindowModel>
                     {
                         var trace = string.Join(NewLine, AsyncLogger.logs);
 
-                        partTrace = new FlexColumn(WidthFull, Height(200))
+                        partTrace = new FlexColumn(WidthFull, Height(150))
                         {
                             new Label { Text = "Trace" },
 
