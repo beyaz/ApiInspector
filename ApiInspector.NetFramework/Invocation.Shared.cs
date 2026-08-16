2343417f86082fc2422738c0b0bec82fc368210a
Abdullah Beyaztaş <beyaz1404@gmail.com>
2026-08-16 15:41:01 +0300

    .

---
+++ ApiInspector.NetFramework/Invocation.Shared.cs
@@ -0,0 +1,33 @@
+﻿global using static ApiInspector.Mixin;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Threading;
+
+namespace ApiInspector;
+
+static class Mixin
+{
+    internal static void WaitForDebuggerAttach()
+    {
+        while (!Debugger.IsAttached)
+        {
+            Thread.Sleep(100);
+        }
+    }
+}
+
+sealed class LogTextWriter : TextWriter
+{
+    public override Encoding Encoding => Encoding.UTF8;
+
+    public override void Write(string value)
+    {
+        WriteLog(value);
+    }
+
+    public override void WriteLine(string value)
+    {
+        WriteLog(value);
+    }
+}
--- ApiInspector.NetFramework/Program.cs
+++ ApiInspector.NetFramework/Program.cs
@@ -597,26 +597,7 @@ static class Program
         return JsonConvert.SerializeObject(response, jsonSerializerSettings);
     }
 
-    static void WaitForDebuggerAttach()
-    {
-        while (!Debugger.IsAttached)
-        {
-            Thread.Sleep(100);
-        }
-    }
+  
 
-    sealed class LogTextWriter : TextWriter
-    {
-        public override Encoding Encoding => Encoding.UTF8;
-
-        public override void Write(string value)
-        {
-            WriteLog(value);
-        }
-
-        public override void WriteLine(string value)
-        {
-            WriteLog(value);
-        }
-    }
+   
 }
